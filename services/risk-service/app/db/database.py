b84a958543ff5c6aeeb183601c75d12a792aba17
MenaceHecker <thedynamic2508@gmail.com>
2026-04-14 11:36:54 -0400

    Added db config

---
+++ services/risk-service/app/db/database.py
@@ -0,0 +1,13 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg://fraudsense:fraudsense@localhost:5432/fraudsense",
+)
+
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+Base = declarative_base()
